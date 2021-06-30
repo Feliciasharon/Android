5248a42ee9f58f0242467e38e28bb57f98bd2ff3
Feliciasharon <75035335+Feliciasharon@users.noreply.github.com>
2021-06-30 19:35:42 +0530

    Update foodForm.js

--- Assignment4/src/foodForm.js
+++ Assignment4/src/foodForm.js
@@ -1,50 +1,46 @@
-import React, { useState } from 'react';
-import {
-  StyleSheet,
-  View,
-  TextInput,
-  Text,
-  TouchableOpacity,
-  Image
-} from 'react-native'
-import { useDispatch } from 'react-redux';
+import React, { Component } from 'react';
+import { StyleSheet, View, TextInput, Text, TouchableOpacity, Image } from 'react-native'
+import { connect } from 'react-redux';
 import { addFood } from './actions/food';
 
-const FoodForm = ({ navigation }) => {
+class FoodForm extends Component{
 
-  const [food, setFood] = useState('')
+static navigationOptions = {
+title:'Home',
+headerTintColor:'white',
+headerStyle: {
+backgroundColor:'blue',
+},
+};
+state = {
+food:null
+}
 
-  const dispatch = useDispatch();
-
-  const submitFood = (food) => dispatch(addFood(food))
-
+render(){
   return (
     <View style={styles.container}>
 
       <Text style={styles.title}>Food Form</Text>
       <TextInput
-        value={food}
+        value={this.state.food}
         placeholder='Name'
         style={styles.foodInput}
-        onChangeText={(food) => setFood(food)}
+        onChangeText={(food) => this.setState({ food })}
       />
       <TouchableOpacity
         style={{ marginBottom: 16 }}
-        onPress={() => {
-          submitFood(food)
-          setFood('')
-        }}>
+        onPress={() => this.props.add(this.state.food)}>
         <Text style={{ fontSize: 22, color: '#5fc9f8' }}>Submit</Text>
       </TouchableOpacity>
       <TouchableOpacity
         style={{ marginBottom: 16 }}
-        onPress={() =>
-          navigation.navigate('FoodList')}>
+        onPress={() => this.props.navigation.navigate('FoodList')}>
         <Text style={{ fontSize: 22, color: 'white' }}>Go to FoodList</Text>
       </TouchableOpacity>
     </View>
   );
 }
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -57,7 +53,7 @@ const styles = StyleSheet.create({
     fontSize: 48,
     marginBottom: 30,
     marginTop: 16,
-    color: 'plum'
+    color: 'white'
   },
   foodInput: {
     fontSize: 24,
@@ -77,4 +73,17 @@ const styles = StyleSheet.create({
   }
 });
 
-export default FoodForm;
+const mapStateToProps = (state) => {
+console.log(state);
+return{
+foods: state.foodReducer.foodList
+}
+}
+
+const mapDispatchToProps = (dispatch) => {
+return{
+add: (food) => dispatch(addFood(food)),
+}
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(FoodForm);
