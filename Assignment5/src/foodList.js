3b913dda1389de2c39b9089664327af68df64dd3
Feliciasharon <75035335+Feliciasharon@users.noreply.github.com>
2021-06-30 19:36:24 +0530

    Update foodList.js

--- Assignment5/src/foodList.js
+++ Assignment5/src/foodList.js
@@ -1,82 +1,72 @@
-import React, { Component } from 'react';
-import { StyleSheet, FlatList, View, TextInput, Text, TouchableOpacity, Image } from 'react-native'
-import { connect } from 'react-redux';
+import React from 'react';
+import {
+  StyleSheet, Text,
+  FlatList, View, TouchableOpacity
+} from 'react-native';
+import { ListItem, Icon } from 'react-native-elements'
+import { useDispatch, useSelector } from 'react-redux';
 import { delFood } from './actions/food';
-import { ListItem, Icon } from 'react-native-elements';
-class FoodList extends Component{
 
-               static navigationOptions = {
-               title:'Food List',
-               headerTintColor:'yellow',
-               headerStyle: {
-               backgroundColor:'blue',
-               },
-               };
-  render(){
+const FoodList = () => {
 
-  return (
-  <View>
-               <FlatList style={styles.listContainer}
-                     data= {this.props.foods}
-                     keyExtractor={(item, index) => item.key.toString()}
-                     renderItem={({ item }) => {
-                     return(
-                                         <TouchableOpacity>
-                                          <View style={styles.row}>
-                                          <Text style ={styles.title}>{item.name}</Text>
-                                          <TouchableOpacity onPress ={()=> this.props.del(item.key)}>
-                                          <Icon name='delete' size={35} color="black" />
-                                          </TouchableOpacity>
-                                          </View>
-                                          </TouchableOpacity>
-                                     );
+  const dispatch = useDispatch();
 
-              }}
-          />
-  </View>
+  const delCurrent = (key) => dispatch(delFood(key))
+
+  const foods = useSelector(state => state.foodReducer.foodList)
+
+  return(
+
+             <View>
+             <FlatList style={styles.listContainer}
+                   data= {foods}
+                   keyExtractor={(item, index) => item.key.toString()}
+                   renderItem={({ item }) => {
+                   return(
+                                       <TouchableOpacity>
+                                        <View style={styles.row}>
+                                        <Text style ={styles.title}>{item.name}</Text>
+                                        <TouchableOpacity onPress ={()=> delCurrent(item.key)}>
+                                        <Icon name='delete' size={35} color="black" />
+                                        </TouchableOpacity>
+                                        </View>
+                                        </TouchableOpacity>
+                                   );
+
+            }}
+        />
+</View>
   );
-}
+
 };
+
 const styles = StyleSheet.create({
   listContainer: {
-    backgroundColor: 'pink',
-    padding: 16
+//backgroundColor: 'green',
+    padding: 20
   },
   listText: {
-    fontSize: 30,
-    color:'black'
+  color:'red',
+    fontSize: 30
   },
-   row:{
-            flexDirection:'row',
-            justifyContent :'space-between',
-            paddingVertical:10,
-            paddingHorizontal:10,
-           // borderTopWidth:1,
-            borderBottomWidth:1,
-            borderColor: 'white',
-            backgroundColor:'peachpuff'
-        },
-        title:{
+  row:{
+          flexDirection:'row',
+          justifyContent :'space-between',
+          paddingVertical:10,
+          paddingHorizontal:10,
+         // borderTopWidth:1,
+          borderBottomWidth:1,
+          borderColor: 'white',
+          backgroundColor:'peachpuff'
+      },
+      title:{
 
-            marginTop:10,
-            color: 'black',
+          marginTop:10,
+          color: 'black',
 
-            //fontWeight: 'bold',
-             fontSize: 25,
-        },
+          //fontWeight: 'bold',
+           fontSize: 25,
+      },
 });
 
-const mapStateToProps = (state) => {
-console.log(state);
-return{
-foods: state.foodReducer.foodList
-}
-}
-
-const mapDispatchToProps = (dispatch) => {
-return{
-del: (key) => dispatch(delFood(key)),
-}
-};
-
-export default connect(mapStateToProps,mapDispatchToProps)(FoodList);
+export default FoodList;
